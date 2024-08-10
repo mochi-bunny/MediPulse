24c2e20293857ed7354c8709a9d5efc962c05990
Megha Manoj <manojmegha2002@gmail.com>
2024-08-10 21:59:23 +0400

    Update sheet.js

--- pages/api/sheet.js
+++ pages/api/sheet.js
@@ -7,9 +7,9 @@ async function  handler(req, res) {
         const auth= new google.auth.GoogleAuth({
             projectId: "join-waitlist-431213",
         credentials:{
-            client_email: process.env.CLIENT_EMAIL,
-            client_id: process.env.CLIENT_ID,
-            private_key : process.env.PRIVATE_KEY
+            client_email: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+            client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+            private_key : process.env.NEXT_PUBLIC_PRIVATE_KEY
         },
         scopes:[
             'https://www.googleapis.com/auth/drive',
